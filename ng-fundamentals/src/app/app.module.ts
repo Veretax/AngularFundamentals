566d01be935efa7e2b8413ae7156dd20741eaf77
Tim Western <westerntc@montgomerycountyva.gov>
2022-06-10 17:02:37 -0400

    part 1 of this

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
-
+import { EventsListComponent } from './events/events-list.component'
+import { EventDetailsComponent } from './events/event-details.component';
 @NgModule({
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventDetailsComponent
   ],
   imports: [
     BrowserModule
---
+++ ng-fundamentals/src/app/events/event-details.component.html.vb
@@ -0,0 +1,5 @@
+﻿
+Partial Class src_app_events_event_details_component
+    Inherits System.Web.UI.Page
+
+End Class
