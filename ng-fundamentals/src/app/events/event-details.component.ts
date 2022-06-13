8946aa5d756b7481f18bd401f13289bd5773c37e
Tim Western <westerntc@montgomerycountyva.gov>
2022-06-13 10:47:52 -0400

    example of styling in component, from practice

--- ng-fundamentals/src/app/events/event-details.component.ts
+++ ng-fundamentals/src/app/events/event-details.component.ts
@@ -3,6 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'event-details',
   templateUrl: './event-details.component.html',
+  styles: [`
+    .outer { display: inline-block; }
+    .inner { margin-top: 30px; }
+    .values { margin-left: 10px; }
+    div.event-label { font-weight: 900; }
+  `]
 })
 export class EventDetailsComponent {
   event = {name:'ngConf 2025', date: '3/1/2025', time: '8am', location: {address: '123 Main St', city: 'Salt Lake City, UT', country: 'USA'}}
