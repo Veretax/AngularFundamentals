8dbe557e63ad71c53c7d6a28ceb381ac0b7376dc
Tim Western <veretax@gmail.com>
2022-06-13 00:14:46 -0400

    Started working on second practice, but ran into a bug

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component'
 import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
 import { EventDetailsComponent } from './events/event-details.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { EventAddressComponent } from './events/event-address.component';
 
 @NgModule({
   declarations: [
+    AppComponent,
     EventsAppComponent,
     EventsListComponent,
-    AppComponent,
     EventDetailsComponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    EventAddressComponent
   ],
   imports: [
     BrowserModule
