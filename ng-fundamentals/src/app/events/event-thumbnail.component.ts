8f28fc9f3534c375fab30f8faa529c0e27c0d312
Tim Western <westerntc@montgomerycountyva.gov>
2022-06-13 16:36:11 -0400

    isEarlyStart function 1 example

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -6,11 +6,10 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     <div class="well hoverwell thumbnail">
       <h2>{{event?.name}}</h2>
       <div>Date: {{event?.date}}</div>
-      <div>Time: {{event?.time}}</div>
-      <div [ngSwitch]="event?.time">
-        <span *ngSwitchCase="'8:00 am'">Early Start</span>
-        <span *ngSwitchCase="'10:00 am'">Late Start</span>
-        <span *ngSwitchDefault>Normal Start</span>
+      <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
       </div>
       <div>Price: \${{ event?.price }}</div>
       <div [hidden]="!event?.location">
@@ -24,6 +23,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     </div>
   `,
   styles: [`
+    .green { color: #003300 !important; }
+    .bold { font-weight: bold; }
     .pad-left { margin-left: 10px; }
     .well div  { color: #bbb; }
     .thumbnail { min-height: 210px; }
@@ -31,4 +32,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 })
 export class EventThumbnailComponent {
   @Input() event: any
+
+  getStartTimeClass() {
+    const isEarlyStart = this.event && this.event.time === '8:00 am';
+    return { green: isEarlyStart, bold: isEarlyStart}
+  }
 }
