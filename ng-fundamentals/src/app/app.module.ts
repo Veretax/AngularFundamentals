0d357264869ce4ce685db1fd557a5095db4cb4cf
Tim Western <veretax@gmail.com>
2022-06-12 21:37:37 -0400

    Completed Test practice

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component'
+//import { EventsAppComponent } from './events-app.component';
+//import { EventsListComponent } from './events/events-list.component'
+import { AppComponent } from './app.component';
 import { EventDetailsComponent } from './events/event-details.component';
 @NgModule({
   declarations: [
-    EventsAppComponent,
-    EventsListComponent,
+//    EventsAppComponent,
+//    EventsListComponent,
+    AppComponent,
     EventDetailsComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [EventsAppComponent]
+  bootstrap: [
+//    EventsAppComponent
+    AppComponent
+  ]
 })
 export class AppModule { }
--- ng-fundamentals/src/app/events/event-details.component.html.vb
+++
@@ -1,5 +0,0 @@
-﻿
-Partial Class src_app_events_event_details_component
-    Inherits System.Web.UI.Page
-
-End Class
