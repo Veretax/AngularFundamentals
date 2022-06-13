51c146a1366a5cb67254e71cbe2d87dc5acbf0e3
Tim Western <veretax@gmail.com>
2022-06-12 23:52:10 -0400

    Sample passing data into a component

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -5,12 +5,15 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component'
 import { AppComponent } from './app.component';
 import { EventDetailsComponent } from './events/event-details.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     AppComponent,
-    EventDetailsComponent
+    EventDetailsComponent,
+    EventThumbnailComponent
   ],
   imports: [
     BrowserModule
