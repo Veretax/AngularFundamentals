2e1d228df17a5c9ba1de4e04d6cabebcc3bec620
Tim Western <westerntc@montgomerycountyva.gov>
2022-06-14 14:29:45 -0400

    moved event service declaration to public for use in template

--- ng-fundamentals/src/app/events/events-list.component.ts
+++ ng-fundamentals/src/app/events/events-list.component.ts
@@ -3,21 +3,21 @@ import { EventService } from "./shared/event.service";
 
 @Component({
   selector: 'events-list',
-  //templateUrl: 'events-list.component.html',
-  template: `
-    <div>
-      <h1>Upcoming Angular Events</h1>
-      <hr />
-          <div class="legend">
-            <span class="in-person">In Person</span> <span class="online">Online</span> <span class="hybrid">Hybrid</span> <span class="tbd">TBD</span>
-          </div>
-      <div class="row">
-        <div *ngFor="let event of events" class="col-md-5">
-          <event-thumbnail [event]="event"></event-thumbnail>
-        </div>
-      </div>
-    </div>
-  `,
+  templateUrl: 'events-list.component.html',
+  //template: `
+  //  <div>
+  //    <h1>Upcoming Angular Events</h1>
+  //    <hr />
+  //        <div class="legend">
+  //          <span class="in-person">In Person</span> <span class="online">Online</span> <span class="hybrid">Hybrid</span> <span class="tbd">TBD</span>
+  //        </div>
+  //    <div class="row">
+  //      <div *ngFor="let event of events" class="col-md-5">
+  //        <event-thumbnail [event]="event"></event-thumbnail>
+  //      </div>
+  //    </div>
+  //  </div>
+  //`,
   styles: [`
     div.legend { padding-bottom: 15px; padding-left: 15px;}
     .legend span { padding-right: 30px; }
@@ -27,10 +27,12 @@ import { EventService } from "./shared/event.service";
     .tbd { color: #aaa; }
     .thumbnail { margin-top:10px; padding-top:5px; }
     .label { font-size:14px; margin-bottom:10px; display:inline-block; }
+    th, td { font-size: 16px; padding:5px 10px;}
   `]
 })
 
 export class EventsListComponent implements OnInit{
+  eventService: EventService;
   events: any[] = [];
   // events = [
   //  { name: 'Angular Connect', date: '9/26/2036', time: '10am', location: { address: '1 London Road ', city: 'London', country: 'England' }, format: "InPerson" },
@@ -38,7 +40,8 @@ export class EventsListComponent implements OnInit{
   //  { name: 'Future Conf (Location/Url TBD)', date: '6/10/2037', time: '8am' },
   //  { name: 'UN Angular Summit', date: '6/10/2037', time: '8am', location: { address: 'The UN Angular Center', city: 'New York', country: 'USA' }, format: "InPerson" }
   //]
-  constructor(private eventService: EventService) {
+  constructor(eventService: EventService) {
+    this.eventService = eventService;
   }
 
   ngOnInit() {
